824d91c6553f318e06025fc900318cfda53ebe75
Rahul <rahulgundiya34@gmail.com>
2020-04-17 20:07:09 +0530

    Setup-Project

---
+++ burger-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+    <p>Hii Developer</p>
+    </div>
+  );
+}
+
+export default App;
