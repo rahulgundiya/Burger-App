80c83ee950b587afcce84f59587326c5fdf44738
Rahul <rahulgundiya34@gmail.com>
2020-04-22 18:39:59 +0530

    Creating SideDrawer

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactAux from '../../hoc/ReactAux'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.module.css'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 const layout = (props) => (
     <ReactAux>
    <Toolbar />
+   <SideDrawer/>
     <main className={classes.Content}> {props.children} </main>
     </ReactAux> 
 
