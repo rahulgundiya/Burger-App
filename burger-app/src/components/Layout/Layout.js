e9034c692c7ecf3a0aff56025c96f3904091504b
Rahul <rahulgundiya34@gmail.com>
2020-04-22 19:55:16 +0530

    Reusing Backdrop

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactAux from '../../hoc/ReactAux'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.module.css'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const layout = (props) => (
-    <ReactAux>
-   <Toolbar />
-   <SideDrawer/>
-    <main className={classes.Content}> {props.children} </main>
-    </ReactAux> 
+class Layout extends Component {
+    state={
+        showSlideDrawer:true
+    }
+    sideDrawerClosedHandler=()=>{
+     this.setState({showSlideDrawer:false})
+    }
+    render(){
+        return(
+            <ReactAux>
+            <Toolbar />
+            <SideDrawer 
+            open={this.state.showSlideDrawer}
+            closed={this.sideDrawerClosedHandler}/>
+             <main className={classes.Content}> 
+             {this.props.children} </main>
+             </ReactAux> 
+         
 
+        )
+    }
+}
+    
     
 
-)
-export default layout;
+export default Layout;
--- burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,9 +2,18 @@ import React from 'react'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import ReactAux from '../../../hoc/ReactAux'
 const sideDrawer=(props)=>{
+    let attachedClass=[classes.SideDrawer , classes.Close]
+    if(props.open)
+    {
+attachedClass=[classes.SideDrawer , classes.Open]
+    }
     return (
-        <div className={classes.SideDrawer}>
+        <ReactAux>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClass.join(' ')}>
             <div className={classes.Logo}>
         <Logo />
         </div>
@@ -12,6 +21,7 @@ const sideDrawer=(props)=>{
             <NavigationItems/>
         </nav>
         </div>
+        </ReactAux>
     )
 }
 export default sideDrawer
