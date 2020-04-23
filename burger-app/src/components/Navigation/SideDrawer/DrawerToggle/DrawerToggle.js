ce30d502749cc375790ea94f399ead721717b624
Rahul <rahulgundiya34@gmail.com>
2020-04-23 12:44:28 +0530

    Hamburger icon add on the place of menu

--- burger-app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger-app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import classes from './DrawerToggle.module.css'
 const drawerToggle=(props)=>(
-<div onClick={props.clicked}>MENU</div>
+<div className ={classes.DrawerToggle } onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+     </div>
 )
 export default drawerToggle;
