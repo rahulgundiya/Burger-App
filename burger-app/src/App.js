6ef6fb0c71ab72d3e0b4a4fec7faf22ed05fd447
Rahul <rahulgundiya34@gmail.com>
2020-04-30 19:16:41 +0530

    Building the checkout container

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -2,24 +2,27 @@ import React, {Component} from 'react';
 //import classes from './App.module.css';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './container/BuilderBurger/BurgerBuilder'
+import Checkout from './container/Checkout/Checkout'
 class  App extends Component {
-  state ={
-    show:true
-  }
-  componentDidMount()
-  {
-    setTimeout(()=>{
-      this.setState({show:false})
-    },5000);
+  // state ={
+  //   show:true
+  // }
+  // componentDidMount()
+  // {
+  //   setTimeout(()=>{
+  //     this.setState({show:false})
+  //   },5000);
   
-  }
-  render() {
+  // }
+  render() {  
   
 
    return (
     <div >
-      <Layout>  
-       { this.state.show?<BurgerBuilder/>:null }
+      <Layout> 
+        <BurgerBuilder/> 
+        <Checkout/>
+       {/* { this.state.show?<BurgerBuilder/>:null } */}
       </Layout>
        </div>
    )
---
+++ burger-app/src/components/order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.module.css'
+const checkoutSummary =(props)=>{
+    return(
+    <div className={classes.checkoutSummary}>
+<h1>we hope it tastes well.!!</h1>
+<div style={{width:'100%' , margin:"auto"}}>
+    <Burger ingredients={props.ingredients}/>
+</div>
+<div className={classes.Button}>
+<Button btnType="Danger"  clicked>CANCEL</Button>
+<Button btnType="Success" clicked>CONTINUE</Button>
+</div>
+</div>
+    )
+}
+
+export default checkoutSummary;
