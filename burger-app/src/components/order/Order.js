8b636593a51d28319edb57c599381a4d862d8fa7
Rahul <rahulgundiya34@gmail.com>
2020-05-01 20:58:36 +0530

    OutPuting the order

--- burger-app/src/components/order/Order.js
+++ burger-app/src/components/order/Order.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import classes from './Order.module.css'
 
-const order=(props)=>(
-    <div className={classes.Order}>
-        <p>ingredients Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p> 
+const order=(props)=>{
+    const ingredients=[];
+    for(let ingredientName in props.ingredients)
+    {
+        ingredients.push({
+            name:ingredientName,
+            ammout:props.ingredients[ingredientName]
+        })
+    }
+        const ingredientOutput = ingredients.map(ig=>{
+            return<span style={{
+                margin:'0 8px',
+                textTransform:'capitalize',
+                display:'inline-block',
+                border:'1px solid #ccc',
+                padding : '5px'
+            }} key={ig.name}>{ig.name} ({ig.ammout})</span>
+        })
+    
+    return(<div className={classes.Order}>
+        <p>{ingredientOutput}</p>
+<p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p> 
 
     </div>
-    )
+)
+    }
+    
+    
 
 
 export default order;
