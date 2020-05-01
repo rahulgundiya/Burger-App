076826ec1da48f0027e046c5a61143e1983a0eff
Rahul <rahulgundiya34@gmail.com>
2020-05-01 20:29:07 +0530

    Fetching Orders from server

--- burger-app/src/container/Orders/Orders.js
+++ burger-app/src/container/Orders/Orders.js
@@ -1,7 +1,29 @@
 import React , { Component } from "react";
 import Order from '../../components/order/Order'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component{
+    state={
+        orders:[],
+        loading:true
+    }
+    componentDidMount(){
+       const fetchedData =[];
+        axios.get('https://burger-app-8f106.firebaseio.com/orders.json')
+        .then(response=>{
+         for(let key in response.data){
+          fetchedData.push({
+              ...response.data[key],
+               id:key});
+         }
+            this.setState({loading:false , orders:fetchedData})
+            
+        })
+        .catch(err=>{
+            this.setState({loading:false});
+        })
+    }
     render(){
         return(
             <div>
@@ -13,4 +35,4 @@ class Orders extends Component{
 
 
 }
-export default Orders;
+export default withErrorHandler(Orders , axios);
