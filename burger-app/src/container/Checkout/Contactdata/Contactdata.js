18e7f4a49105fc3bb34440c68c2f759780d4c3a1
Rahul <rahulgundiya34@gmail.com>
2020-05-05 13:16:38 +0530

    Fixing a Common Validation Gotcha

--- burger-app/src/container/Checkout/Contactdata/Contactdata.js
+++ burger-app/src/container/Checkout/Contactdata/Contactdata.js
@@ -113,18 +113,18 @@ orderHandler=(event)=>{
     }
     
     checkValidity(value , rules){
-        let isValid=false;
+        let isValid=true;
         if(rules.required)
         {
-         isValid =value.trim() !== '';
+         isValid = value.trim() !== '' && isValid;
         }
         if(rules.minLength)
         {
-            isValid = value.length >=rules.minLength
+            isValid = value.length >=rules.minLength &&isValid
         }
         if(rules.maxLength)
         {
-            isValid=value.length <=rules.maxLength
+            isValid=value.length <=rules.maxLength && isValid
         }
         return isValid;
 
