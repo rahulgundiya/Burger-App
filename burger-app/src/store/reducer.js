781cb73d296a499b7365bb6393cee424fc82d74c
Rahul <rahulgundiya34@gmail.com>
2020-05-19 17:31:49 +0530

    Finishing the reducer for ingredients

--- burger-app/src/store/reducer.js
+++ burger-app/src/store/reducer.js
@@ -1,11 +1,39 @@
 import * as actionTypes from '../store/action'
 
 const initialState={
-    ingredients:null,
+    ingredients:{
+        cheese:0,
+        meet:0,
+        bacon:0,
+        butter:0,
+        salad:0
+    },
     totalPrice:4
 }
 const reducer=(state=initialState,action)=>{
+            switch(actionTypes)
+            {
+                case(actionTypes.ADD_INGREDIENT):
+                 return{
+                     ...state,
+                     ingredients:{
+                         ...state.ingredients,
+                     [action.ingredientName]:state.ingredients[action.ingredientName]+1
+                     }
+                    }
+                 case(actionTypes.REMOVE_INGREDIENT):
+                 return{
+                    ...state,
+                    ingredients:{
+                        ...state.ingredients,
+                    [action.ingredientName]:state.ingredients[action.ingredientName]-1
+                    }
 
+                 }   
+                 default:
+                     return state
+
+                }
 }
 
 export default reducer;
