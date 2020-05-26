68c8bd9742e44a2bd5548728ebe31976070d727b
Rahul <rahulgundiya34@gmail.com>
2020-05-26 20:33:27 +0530

    Changing Order of ingredients manually

--- burger-app/src/store/reducers/burgerBuilder.js
+++ burger-app/src/store/reducers/burgerBuilder.js
@@ -48,7 +48,13 @@ const reducer=(state=initialState,action)=>{
                  case actionTypes.SET_INGREDIENTS:
                  return {
                      ...state,
-                     ingredients:action.ingredients,
+                     ingredients:{
+                         salad:action.ingredients.salad,
+                         bacon:action.ingredients.bacon,
+                         cheese:action.ingredients.cheese,
+                         meat:action.ingredients.meat,
+                         butter:action.ingredients.butter
+                     },
                      error:false
                  }
                  case actionTypes.FETCH_INGREDIENTS_FAILED:
