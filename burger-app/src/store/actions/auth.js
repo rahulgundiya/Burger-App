8e0b7ac68934e89cc6828d0c4aa54ee92ce0bf6f
Rahul <rahulgundiya34@gmail.com>
2020-06-12 22:07:42 +0530

    Getting tokens in backend using FireBase

--- burger-app/src/store/actions/auth.js
+++ burger-app/src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes'
-
+import axios from 'axios'
 export const authStart =()=>{
     return {
         type:actionTypes.AUTH_START
@@ -22,5 +22,19 @@ export const authFail =(error)=>{
 export const auth=(email,password)=>{
     return dispatch=>{
         dispatch(authStart())
+        const authData ={
+            email:email,
+            password:password,
+            returnSecureToken:true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVH61NpO3suwIon1jBwlAijsQ9iKpZNqc',authData)
+       .then(res=>{
+           console.log(res)
+           dispatch(authSuccess(res.data))
+       })
+       .catch(err=>{
+           console.log(err);
+           dispatch(authFail(err))
+       })
     }
 }
